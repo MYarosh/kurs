5daf1b03557268adc4ec620c51056234236d1de5
Daria <8117755@gmail.com>
2020-12-06 21:11:59 +0300

    new-MAIN&index

--- src/main/java/com/maxyar/kurs/controllers/MainController.java
+++ src/main/java/com/maxyar/kurs/controllers/MainController.java
@@ -1,16 +1,14 @@
 package com.maxyar.kurs.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
     public String greeting() {
-        return "main";
+        return "index";
     }
 
 }
